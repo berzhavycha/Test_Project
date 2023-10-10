9569a8b2e7d1fba671eded4de23cae0105f9c505
Berzhavych Andriy <andriyberzhavich0@gmail.com>
2023-10-10 18:50:01 +0300

    removed unnecessary line

--- src/functionality/getClosestPlacesByCoords.ts
+++ src/functionality/getClosestPlacesByCoords.ts
@@ -5,7 +5,6 @@ export async function placeSearch(latitude: number, longitude: number) {
         do {
             const searchParams = new URLSearchParams({
                 ll: `${latitude},${longitude}`,
-                open_now: 'true',
                 radius: `${2000 * i}`,
             });
 
